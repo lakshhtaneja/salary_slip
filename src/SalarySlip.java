1802a60367763ab9ed536495a3f5eb98140831f8
laksh <lakshtaneja77@gmail.com>
2024-02-08 14:06:18 +0530

    SRP by chaining functions

--- src/SalarySlip.java
+++ src/SalarySlip.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 class SalarySlip{
-    public static void main(String[] args) {
-        // input
+
+    void input(){
         Scanner scanner= new Scanner(System.in);
 
         System.out.println("Enter your Id: ");
@@ -14,10 +14,12 @@ class SalarySlip{
 
         System.out.println("Enter your basic Salary: ");
         double basicSalary = scanner.nextDouble();
-
+        compute(id, name, basicSalary);
         scanner.close();
 
-        //compute
+    }
+
+    void compute(int id, String name, double basicSalary){
         double hra = basicSalary * 0.5;
         double da = basicSalary * 0.2;
         double ta = basicSalary * 0.4;
@@ -27,8 +29,11 @@ class SalarySlip{
 
         double grossSalary = basicSalary + hra + da + ta + ma;
 
+        print(id, name, basicSalary, hra, pf, da, ta, ma);
 
-        //console output
+    }
+
+        void print(int id, String name, double basicSalary, double hra, double pf, double da, double ta, double ma){
         System.out.println("Id: " + id);
         System.out.println("Name: " + name + "\n");
         System.out.println("Basic Salary: " + basicSalary);
@@ -39,5 +44,9 @@ class SalarySlip{
         System.out.println("DA: " + da);
         System.out.println("TA: " + ta);
         System.out.println("MA: " + ma);
+    }
+
+    public static void main(String[] args) {
+     
     }
 }
